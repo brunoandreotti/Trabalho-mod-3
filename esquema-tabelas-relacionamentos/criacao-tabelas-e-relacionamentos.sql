af72ccd67186fbf0d98ebcec1c53bb3838c4070c
Bruno Andreotti <brunoandreotti@outlook.com>
2022-02-07 14:24:03 -0300

    Update criacao-tabelas-e-relacionamentos.sql

--- esquema-tabelas-relacionamentos/criacao-tabelas-e-relacionamentos.sql
+++ esquema-tabelas-relacionamentos/criacao-tabelas-e-relacionamentos.sql
@@ -9,14 +9,6 @@ CREATE TABLE `games` (
   `HOME_TEAM_WINS` int
 );
 
-CREATE TABLE `game_details` (
-  `GAME_ID` int,
-  `TEAM_ID` int,
-  `TEAM_ABBREVIATION` text,
-  `PLAYER_ID` int,
-  `PLAYER_NAME` text
-);
-
 CREATE TABLE `players` (
   `PLAYER_NAME` text,
   `TEAM_ID` int,
@@ -34,8 +26,5 @@ ALTER TABLE `games` ADD FOREIGN KEY (`HOME_TEAM_ID`) REFERENCES `teams` (`TEAM_I
 
 ALTER TABLE `games` ADD FOREIGN KEY (`VISITOR_TEAM_ID`) REFERENCES `teams` (`TEAM_ID`);
 
-ALTER TABLE `game_details` ADD FOREIGN KEY (`TEAM_ID`) REFERENCES `teams` (`TEAM_ID`);
+ALTER TABLE `player` ADD FOREIGN KEY (`TEAM_ID`) REFERENCES `teams` (`TEAM_ID`);
 
-ALTER TABLE `game_details` ADD FOREIGN KEY (`GAME_ID`) REFERENCES `games` (`GAME_ID`);
-
-ALTER TABLE `game_details` ADD FOREIGN KEY (`PLAYER_ID`) REFERENCES `players` (`PLAYER_ID`);
